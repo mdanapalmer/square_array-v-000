fb68aa47a695ff7386456622165844f8f03c1bc2
Dana Palmer <roiai4@hotmail.com>
2018-10-07 05:18:45 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,17 @@
+#def square_array(array)
+ # answer = []
+  #array.each do |square|
+  #answer << square ** 2
+#end
+#answer
+#end
+
+#  #answer = []
+  #array.collect {|square| answer << square ** 2}
+#end
+
 def square_array(array)
   answer = []
-  array.each do |square| 
-  answer << square ** 2 
-end
-answer
+  array.each {|square| answer << (square ** 2)}
+  answer
 end
