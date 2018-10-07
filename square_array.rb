5a4c98b7ed9f862dd626dc38daff88bbde7ceb57
Dana Palmer <roiai4@hotmail.com>
2018-10-07 05:10:54 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
+#def square_array(array)
+ # answer = []
+  #array.each do |square|
+  #answer << square ** 2
+#end
+#answer
+#end
+
 def square_array(array)
   answer = []
-  array.each do |square| 
-  answer << square ** 2 
-end
-answer
+  array.collect {|square| answer << square ** 2}
 end
